38d353ba01b9aca04f3f2c70475dec1cdd725fb9
Alessandro Semeraro <alexei.semeraro@gmail.com>
2024-06-07 14:14:15 +0200

    typecasting done

--- training_threads/intro.c
+++ training_threads/intro.c
@@ -9,19 +9,18 @@ pthread_detach, pthread_join, pthread_mutex_init,
 pthread_mutex_destroy, pthread_mutex_lock,
 pthread_mutex_unlock*/
 
-int x = 2;
 
-void *routine1()
+void *routine1(void *arg)
 {
-	printf("value of x in t1: %d\n", x);
+	printf("value of x in t1: %d\n", *(int *)arg);
 	return (NULL);
 }
 
-void *routine2()
+void *routine2(void *arg)
 {
 	sleep(2);
-	x++;
-	printf("vale of x in t2: %d\n", x);
+	*(int *)arg += 1;
+	printf("vale of x in t2: %d\n", *(int *)arg);
 	return (NULL);
 }
 
@@ -30,15 +29,16 @@ int main(int argc, char **argv)
 	pthread_t t1, t2;
 	(void)argv;
 	(void)argc;
+	int x = 2;
 
 
-	if (pthread_create(&t1, NULL, &routine1, NULL) != 0)
+	if (pthread_create(&t1, NULL, &routine1, &x) != 0)
 		return (1);
-	if (pthread_create(&t2, NULL, &routine2, NULL) != 0)
-		return (1);
+	if (pthread_create(&t2, NULL, &routine2, &x) != 0)
+		return (2);
 	if (pthread_join(t1, NULL) != 0)
-		return (2);
+		return (3);
 	if (pthread_join(t2, NULL) != 0)
-		return (2);
+		return (4);
 	return (0);
 }
