4529272b4056554d72f62171f08075c55fca8872
Alessandro Semeraro <alexei.semeraro@gmail.com>
2024-06-11 14:28:04 +0200

    introduced the timestamp

--- src/main.c
+++ src/main.c
@@ -13,12 +13,13 @@ typedef struct s_program
 	int				time_to_eat;
 	int				time_to_sleep;
 	int				is_dead;
+	int				meals_needed;
+	struct timeval	g_time;
 }	t_program;
 
 typedef struct s_fork
 {
 	pthread_mutex_t lock;
-	bool			is_taken;
 	int				fork_id;
 }	t_fork;
 
@@ -41,34 +42,23 @@ void *routine(void *philos)
 	
 	new_philos = (t_philo *)(philos);
 
-	printf("new_philo\nf_id: %lu\ntv: %p\nthread: %p\nfork_right: %p\nfork_left: %p\ndata: %p\n", new_philos->f_id, &new_philos->tv, &new_philos->thread, new_philos->fork_right, new_philos->fork_left, new_philos->data);
 	pthread_mutex_lock(&new_philos->fork_left->lock);
 	pthread_mutex_lock(&new_philos->fork_right->lock);
-	if (new_philos->fork_left->is_taken || new_philos->fork_right->is_taken)
-	{
-		pthread_mutex_unlock(&new_philos->fork_left->lock);
-		pthread_mutex_unlock(&new_philos->fork_right->lock);
-		// think for x amount of time
-	}
-	else
-	{
-		new_philos->fork_left->is_taken = true;
-		new_philos->fork_right->is_taken = true;
-		pthread_mutex_unlock(&new_philos->fork_left->lock);
-		pthread_mutex_unlock(&new_philos->fork_right->lock);
-		// eat for x amount of time, then 
-		printf("philo n %ld: has taken the left fork\n", new_philos->f_id);
-		printf("philo n %ld: has taken the right fork\n", new_philos->f_id);
-	}
+	printf("new_philo\nf_id: %lu\ntv: %p\nthread: %p\nfork_right: %p\nfork_left: %p\ndata: %p\n", new_philos->f_id, &new_philos->tv, &new_philos->thread, new_philos->fork_right, new_philos->fork_left, new_philos->data);
+	
+	printf("%ld philo n :%ld has taken the left fork\n", new_philos->data->g_time.tv_sec*1000 + new_philos->data->g_time.tv_usec/1000, new_philos->f_id);
+	printf("%ld philo n :%ld has taken the right fork\n", new_philos->data->g_time.tv_sec*1000 + new_philos->data->g_time.tv_usec/1000, new_philos->f_id);
+	printf("%ld philo n :%ld has taken the right fork\n", new_philos->data->g_time.tv_sec*1000 + new_philos->data->g_time.tv_usec/1000, new_philos->f_id);
+	usleep(200);
 	pthread_mutex_unlock(&new_philos->fork_left->lock);
-
+	pthread_mutex_unlock(&new_philos->fork_right->lock);
 	return NULL;
 
 }
 
 int main(int argc, char **argv)
 {
-	if (argc != 5)
+	if (argc < 5 || argc > 6)
 	{
 		printf("not right amout of argument given\n");
 		return (1);
@@ -78,19 +68,26 @@ int main(int argc, char **argv)
 	t_fork				*forks;
 	int					i;
 
+	//p_data.g_time = 
+	
 	p_data.n_filos = atoi(argv[1]);
 	p_data.time_to_die = atoi(argv[2]);
 	p_data.time_to_eat = atoi(argv[3]);
 	p_data.time_to_sleep = atoi(argv[4]);
+	if (argc == 6)
+		p_data.meals_needed = atoi(argv[5]);
+	else
+		p_data.meals_needed = 0;
+	
 	philosophers = malloc(sizeof(t_philo) *p_data.n_filos);
 	forks = malloc(sizeof(t_fork) *p_data.n_filos);
+	gettimeofday(&p_data.g_time, NULL);
 
 	i = 0;
 	while(i < p_data.n_filos)
 	{
 		forks[i].fork_id = i + 1;
 		pthread_mutex_init(&forks[i].lock, NULL);
-		forks[i].is_taken = false;
 		i++;
 	}
 	i = 0;
@@ -113,15 +110,21 @@ int main(int argc, char **argv)
 		pthread_create(&philosophers[i].thread, NULL, &routine, &philosophers[i]);
 		i++;
 	}
-	int x = 0;
-	while(x < p_data.n_filos)
+	i = 0;
+	while(i < p_data.n_filos)
 	{
-		printf("philo n: %zu, and his fork left_id: %p, and his fork right_id:%p\n", philosophers[x].f_id, philosophers[x].fork_left, philosophers[x].fork_right);
-// 		printf("philo n: %zu, and his fork left_lock: %p, and his fork right_lock: \
-// %p\n", philosophers[x].f_id, philosophers[x].fork_left, philosophers[x].fork_right);
-
-		x++;
+		pthread_join(philosophers[i].thread, NULL);
+		i++;
 	}
+	int x = 0;
+// 	while(x < p_data.n_filos)
+// 	{
+// 		printf("philo n: %zu, and his fork left_id: %p, and his fork right_id:%p\n", philosophers[x].f_id, philosophers[x].fork_left, philosophers[x].fork_right);
+// // 		printf("philo n: %zu, and his fork left_lock: %p, and his fork right_lock: \
+// // %p\n", philosophers[x].f_id, philosophers[x].fork_left, philosophers[x].fork_right);
+
+// 		x++;
+// 	}
 	// free(philosophers);
 	// free(forks);
 
